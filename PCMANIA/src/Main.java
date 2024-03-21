954b5232119574575737ca81752ef099638b2261
Matheus Maciel <maathdesigner@outlook.com>
2024-03-21 20:38:30 -0300

    V0.2

--- PCMANIA/src/Main.java
+++ PCMANIA/src/Main.java
@@ -1,2 +1,14 @@
 public class Main {
+    public static void main(String[] args) {
+
+        SistemaOperacional LinuxUbuntu = new SistemaOperacional("Linux Ubuntu", 32);
+        HardwareBasico P1 = new HardwareBasico("Pentium Core i3 (2200 Mhz)", 500f);
+
+
+
+
+        Computador computador1 = new Computador("Positivo", 3300);
+
+
+    }
 }
