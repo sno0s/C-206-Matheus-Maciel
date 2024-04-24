4e2ca97958a5258f7e017e1894a8cd0d7f7e0866
sno0s <maathdesigner@Outlook.com>
2024-04-24 16:33:22 -0300

    Avaliação 240424

---
+++ AV2-C206-L2/src/Defesa.java
@@ -0,0 +1,3 @@
+public interface Defesa {
+    void Defender();
+}
---
+++ AV2-C206-L2/src/ItemEspecial.java
@@ -0,0 +1,7 @@
+public class ItemEspecial {
+    public String tipo;
+
+    public ItemEspecial(String tipo) {
+        this.tipo = tipo;
+    }
+}
